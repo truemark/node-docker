78be72751c7e48c953bc4598ebe57f61ec635ded
Erik Jensen <365211+erikrj@users.noreply.github.com>
2023-04-15 04:31:30 -0600

    Updating ref

--- local.sh
+++ local.sh
@@ -2,11 +2,11 @@ export NODE_VERSION=$(curl -sSL https://nodejs.org/dist/latest-v16.x/ | grep lin
 echo "Using node version ${NODE_VERSION}"
 
 docker build -t moo --build-arg OS_NAME=alpine --build-arg OS_VERSION=3.17 --build-arg NODE_VERSION=${NODE_VERSION} -f Dockerfile .
-docker build -t moo --build-arg OS_NAME=debian --build-arg OS_VERSION=bookworm --build-arg NODE_VERSION=${NODE_VERSION} -f Dockerfile .
-docker build -t moo --build-arg OS_NAME=amazonlinux --build-arg OS_VERSION=2 --build-arg NODE_VERSION=${NODE_VERSION} -f Dockerfile .
+#docker build -t moo --build-arg OS_NAME=debian --build-arg OS_VERSION=bookworm --build-arg NODE_VERSION=${NODE_VERSION} -f Dockerfile .
+#docker build -t moo --build-arg OS_NAME=amazonlinux --build-arg OS_VERSION=2 --build-arg NODE_VERSION=${NODE_VERSION} -f Dockerfile .
 
-export NODE_VERSION=$(curl -sSL https://nodejs.org/dist/latest-v18.x/ | grep linux-x64.tar.gz | sed -n "s/.*>node-v\(.*\)-linux.*<.*/\1/p")
-echo "Using node version ${NODE_VERSION}"
+#export NODE_VERSION=$(curl -sSL https://nodejs.org/dist/latest-v18.x/ | grep linux-x64.tar.gz | sed -n "s/.*>node-v\(.*\)-linux.*<.*/\1/p")
+#echo "Using node version ${NODE_VERSION}"
 
-docker build -t moo --build-arg OS_NAME=alpine --build-arg OS_VERSION=3.17 --build-arg NODE_VERSION=${NODE_VERSION} -f Dockerfile .
-docker build -t moo --build-arg OS_NAME=debian --build-arg OS_VERSION=bookworm --build-arg NODE_VERSION=${NODE_VERSION} -f Dockerfile .
+#docker build -t moo --build-arg OS_NAME=alpine --build-arg OS_VERSION=3.17 --build-arg NODE_VERSION=${NODE_VERSION} -f Dockerfile .
+#docker build -t moo --build-arg OS_NAME=debian --build-arg OS_VERSION=bookworm --build-arg NODE_VERSION=${NODE_VERSION} -f Dockerfile .
